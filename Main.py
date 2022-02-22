e7c6bac3485f728fe1256c0e2a2989a9205636b0
Andrew Kurtiak <43471736+thearcher555@users.noreply.github.com>
2022-02-22 12:57:47 -0500

    Populated test file and allowed import of text file to python

--- Main.py
+++ Main.py
@@ -1,22 +1,30 @@
+#Bar object to hold all variables
+#Array of bars will be used to index all entries
 class Bar:
-    name = "";
+    name = ""
     maxPopulation = -1
     currentPopulation = -1
-    capacity = .001;
+    capacity = .001
 
     def setMaxPop(num):
-        maxPopulation = num;
+        maxPopulation = num
 
     def calculateCapacity():
         capacity = currentPopulation/maxPopulation
 
     def updatePop(num):
-        currentPopulation = num;
+        currentPopulation = num
         calculateCapacity()
+
+    def setName(newName):
+        name = newName
 
 Bars = []
 
 #Create way to import bar data from text file
+with open ('NewBrunswickBars.txt', 'r') as reader:
+    Bars.append()
+
 
 #Simulation code
 
