3630190f69f85aa7103a119f08a0274e80aa2767
Andrew Kurtiak <43471736+thearcher555@users.noreply.github.com>
2022-03-16 23:10:10 -0400

    fixed bug with Bar initialization, begun design of simulation

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
+import random
+
 #Bar object to hold all variables
 #Array of bars will be used to index all entries
 class Bar:
     # notes: self.currentPopulation should always begin at -1, self.capacity at .001
-    def __init__(self,name,maxPopulation, currentPopulation, capacity):
+    def __init__(self,name,maxPopulation,):
         self.name = str(name)
         self.maxPopulation = int(maxPopulation)
-        self.currentPopulation = int(currentPopulation)
-        self.capacity = int(capacity)
+        self.currentPopulation = int(maxPopulation * .65) #bar fullness configuration
+        self.capacity = .001
 
     def setMaxPop(self, num):
         maxPopulation = num
@@ -23,16 +25,32 @@ class Bar:
 
 Bars = []
 
-#Create way to import bar data from text file
+# Imports lines from text file, splits name and population
+# into Bar object, and is added to Bars array
 with open ('NewBrunswickBars.txt', 'r') as f:
     for line in f:
-        line.split(",")
-        num = type(int(line[1]))
+        line = line.split(",")
+        num = int(line[1])
         temp = Bar(line[0],num)
         Bars.append(temp)
 
 
-#Simulation code
+#Simulation Code
+#Bar starts at some percentage full on construction (see bar constructor to configure)
+#Population is shuffled a series of times to randomize population distribution
+
+#i've been running into a problem with all my ideas for the shuffling
+#the smaller bars fill upo faster than the large ones when randomizing
+#we need to make some sort of 'weighting' on the larger bars so people
+#are more likely to chose them than the smaller ones
+
+print(Bars[5].currentPopulation) #test
+
+
+
+
+
+
 
 
 
