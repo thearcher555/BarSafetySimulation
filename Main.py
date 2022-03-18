10ab18c5f5d31aa9036864df12ee7038ed6187e5
Andrew Kurtiak <43471736+thearcher555@users.noreply.github.com>
2022-03-17 23:02:04 -0400

    modified barChoice to new algorithm, added edge case protection

--- Main.py
+++ Main.py
@@ -74,27 +74,29 @@ class Simulation:
     #picks the 5 median bars based of capacity
     #can be modified later to have an offset
     def barChoice(self):
-        sortedBars = sorted(Bars, key=lambda x: x.capacity)
+        sortedBars = sorted(self.BarsList, key=lambda x: x.capacity)
         chosenBars = []
-        if len(Bars) >= 5:
-            length = int(len(Bars) / 2)
-            chosenBars.append(sortedBars[length+1])
-            chosenBars.append(sortedBars[length+2])
-            chosenBars.append(sortedBars[length])
-            chosenBars.append(sortedBars[length-1])
-            chosenBars.append(sortedBars[length-2])
-        else:
-            chosenBars = Bars
+
+        for x in self.BarsList:
+            if(x.capacity >= .6 & x.capacity <= .8):
+                chosenBars.append(x)
+
+        if len(chosenBars) == 0:
+
+            if len(Bars) >= 5:
+                length = int(len(Bars) / 2)
+                chosenBars.append(sortedBars[length+1])
+                chosenBars.append(sortedBars[length+2])
+                chosenBars.append(sortedBars[length])
+                chosenBars.append(sortedBars[length-1])
+                chosenBars.append(sortedBars[length-2])
+            else:
+                chosenBars = Bars
         
         return chosenBars
 
 #Simulation Code
-#Population is shuffled a series of times to randomize population distribution
-
-#i've been running into a problem with all my ideas for the shuffling
-#the smaller bars fill upo faster than the large ones when randomizing
-#we need to make some sort of 'weighting' on the larger bars so people
-#are more likely to chose them than the smaller ones
+#Population is initialized then redistributed
 
 # helper method to read from text file
 def readBars():
