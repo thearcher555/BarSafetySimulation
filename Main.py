9f3e43db41c388670e877fefea0057475adae0df
cvoy138 <cvoy138@gmail.com>
2022-03-18 22:03:58 -0400

    git pull

--- Main.py
+++ Main.py
@@ -1,19 +1,23 @@
+from configparser import MAX_INTERPOLATION_DEPTH
+from pickle import FALSE, TRUE
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Bar class: objects contain name, max population, current population, and capacity of the bar
 class Bar:
-    # notes: self.currentPopulation should always begin at -1, self.capacity at .001
+    # notes: self.currentPopulation should always begin at 0, self.capacity at .001
     def __init__(self,name,maxPopulation,):
         self.name = str(name)
         self.maxPopulation = int(maxPopulation)
-        self.currentPopulation = int(maxPopulation * .65) #bar fullness configuration
+        self.currentPopulation = 0
         self.capacity = .001
 
     def setMaxPop(self, num):
         maxPopulation = num
 
     def calculateCapacity(self):
-        capacity = self.currentPopulation/self.maxPopulation
+        self.capacity = self.currentPopulation/self.maxPopulation
 
     def updatePop(self, num):
         self.currentPopulation = num
@@ -30,37 +34,133 @@ class Simulation:
     def __init__(self, Bars = []):
         self.BarsList = []
         self.BarsList = Bars
+        self.totalPopulation = 0
 
-    def getPopulations(self):
-        print("\nCurrent Populations of All Bars:")
+    def getBarData(self):
+        print("\n List of all Bars: ")
         for x in self.BarsList:
-            print(x.name + ": " + str(x.currentPopulation))
-
-    #function to choose the 5 'best' bars of an array of bars
-    #picks the 5 median bars based of capacity
-    #can be modified later to have an offset
-    def barChoice(self):
-        sortedBars = sorted(Bars, key=lambda x: x.capacity)
-        chosenBars = [];
-        if len(Bars) >= 5:
-            length = int(len(Bars) / 2)
-            chosenBars.append(sortedBars[length+1])
-            chosenBars.append(sortedBars[length+2])
-            chosenBars.append(sortedBars[length])
-            chosenBars.append(sortedBars[length-1])
-            chosenBars.append(sortedBars[length-2])
-        else:
-            chosenBars = Bars
+            print(x.name + ": " + str(x.currentPopulation) + " out of " + str(x.maxPopulation))
+            x.calculateCapacity()
+            print("   Capacity: " + str(x.capacity) + "\n")
         
+        chosen_dict= {} #chosen dict maps bars to their current cpacities
+        arr = self.barChoice()
+        print("Recommended Bars:")
+        for x in arr:
+            print(x.name + ", ")
+            chosen_dict[x.name]=''
+
+        for x in self.BarsList:
+            if x.name in chosen_dict:
+                x.calculateCapacity()
+                chosen_dict[x.name] = x.capacity
+
+        # displays recommended bars and their current percent capacities
+        barnames = list(chosen_dict.keys())
+        capacities = list(np.round(list(chosen_dict.values()), decimals=2)*100)
+        fig = plt.figure(figsize = (8,5))
+        plt.ylim(0,100)
+        plt.yticks(range(0,101,10))
+        plt.title('Recommended Bars and Their Capacities',fontweight='bold')
+        plt.xlabel('Bars',fontweight='bold')
+        plt.ylabel('Percent Capacity\n(out of 100%)',fontweight='bold')
+        plt.bar(barnames, capacities)
+        plt.show()
+        
+
+            
+    #finds the total number of people that all bars in NB can hold
+    def setTotalPopulation(self):
+        for x in self.BarsList:
+            self.totalPopulation += x.maxPopulation
+
+    # initializes all bars with a population, proportional to their stake in total population of all bars
+    # ensures that higher capacity bars will have more people than smaller bars 
+    # 70% (configurable) of total population is calculated, then distributed among all bars
+    def initializer(self):
+
+        self.setTotalPopulation()
+
+        length = len(self.BarsList)
+        inverseList = list()
+
+        for x in self.BarsList:
+            inverse = x.maxPopulation/self.totalPopulation
+            inverseList.append(inverse)
+
+        movingPeople = int(.70*self.totalPopulation)
+
+        while (movingPeople > 0):
+            index = random.randint(0, length-1)
+            chance = random.random()
+
+            if (self.BarsList[index].currentPopulation >= self.BarsList[index].maxPopulation):
+                continue
+
+            if chance < inverseList[index]:
+                self.BarsList[index].currentPopulation += 1
+                movingPeople -= 1
+
+    # method that simulates people moving from bar to bar, will be run multiple times over the course
+    # of the simulation.  A bar hotness variable is generated to represent desire to leave or go to bar
+    # 40% (configurable) of population is removed, then added back in accordance to bar hotness
+    def shmovement(self):
+        movingPeople = 0
+
+        for x in self.BarsList:
+            tempMoving = int(.40*x.currentPopulation)
+            movingPeople += tempMoving
+            x.currentPopulation -= tempMoving
+
+        length = len(self.BarsList)
+        hotnessList = list()
+
+        for x in self.BarsList:
+            hotness = random.random();
+            hotnessList.append(hotness)
+
+        while (movingPeople > 0):
+            index = random.randint(0, length-1)
+            chance = random.random()
+
+            if (self.BarsList[index].currentPopulation >= self.BarsList[index].maxPopulation):
+                continue
+
+            if chance < hotnessList[index]:
+                self.BarsList[index].currentPopulation += 1
+                movingPeople -= 1
+        
+
+    # Method to generate recommendations for bars based on capacity. Begins with a capacity
+    # window of 65 - 70% (configurable), and increases the size of the window if there are no bars that 
+    # satisfy the threshold. Always will return 3 or 4 bars for the user.
+    # parameters are set so a full bar (95 - 100% capacity) will never be recommended
+    def barChoice(self):
+        sortedBars = sorted(self.BarsList, key=lambda x: x.capacity)
+        chosenBars = []
+        lower = .65
+        upper = .7
+        breakState = TRUE 
+
+        while breakState is TRUE:
+            for x in sortedBars:
+                if(x.capacity >= lower and x.capacity <= upper):
+                    chosenBars.append(x)
+                    sortedBars.remove(x)
+        
+            if len(chosenBars) < 3:
+                if (upper + .05) < 1:
+                    upper += .05
+
+                if (lower - .05) > 0:
+                    lower -= .05
+            else:
+                breakState = FALSE
+            
+        if len(chosenBars) > 4:
+            chosenBars = chosenBars[0:4]
+
         return chosenBars
-
-#Simulation Code
-#Population is shuffled a series of times to randomize population distribution
-
-#i've been running into a problem with all my ideas for the shuffling
-#the smaller bars fill upo faster than the large ones when randomizing
-#we need to make some sort of 'weighting' on the larger bars so people
-#are more likely to chose them than the smaller ones
 
 # helper method to read from text file
 def readBars():
@@ -79,18 +179,20 @@ def readBars():
     return Bars
 
 
+
+
+
+
+
+#main method to make function calls
 if __name__ == "__main__":
     Bars = readBars()
     Simulator = Simulation(Bars)
-    Simulator.getPopulations()
-
-
-
-
-
-
-
-
+    Simulator.initializer()
+    Simulator.getBarData()
+    Simulator.shmovement()
+    print("//////////////////////////////")
+    Simulator.getBarData()
 
 
 
